c824ce390d4fefd38302722fb45f6d7e6bb547c6
Bilal Amjad <69304697+mirza-bilal-amjad@users.noreply.github.com>
2023-10-03 14:59:08 +0500

    Initial Commit | Home Page without carousel

---
+++ cricwick/src/App.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {RootNavigator} from "./navigation";
+
+const App = () => {
+    return (
+        <RootNavigator/>
+    );
+};
+export default App;
+const styles = StyleSheet.create({});
